41e1917527f662f8bc3db747c62279e3784f3299
JhamelThorne <116086991+JhamelThorne@users.noreply.github.com>
2025-03-21 19:34:12 -0400

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,1 +1,9 @@
+terraform {
+  backend"s3"
+    bucket         = "jha-terraform-stat"
+    key            = "vpc-ec2-deploy/terraform.tfstate"
+    region         = " us-east-1"
+    dynamodb_table = "terraform-locks"
+  }
+}
 
