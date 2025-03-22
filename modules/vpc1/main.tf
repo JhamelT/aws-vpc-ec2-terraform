30d59753af19be5b8eb016e4aa2bed96cee59638
JhamelThorne <116086991+JhamelThorne@users.noreply.github.com>
2025-03-22 12:48:13 -0400

    Update main.tf

--- modules/vpc1/main.tf
+++ modules/vpc1/main.tf
@@ -1,1 +1,24 @@
+resource "aws_vpc" "main"  {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    name = "${var.project}-vpc"
+  }
+}
 
+resource "aws_internet_gateway" "igw"  {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    name = "${var.project}-igw"
+  }
+}
+resource "aws_subnet" "public" {
+  vpc_id         = aws_vpc.main.id
+  cidr_block     = var.public_subnet_cidr
+  map_public_ip_on_launch  = true
+  availability_zone        =var.az
+  tags ={  
+      name = "${var.project}-public-subnet"
+      }
+}
