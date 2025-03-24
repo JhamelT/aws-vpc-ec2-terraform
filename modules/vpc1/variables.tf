14cfb51482aefedb12a0b18faf241f2d47c362b9
JhamelThorne <116086991+JhamelThorne@users.noreply.github.com>
2025-03-24 17:45:12 -0400

    Update variables.tf

--- modules/vpc1/variables.tf
+++ modules/vpc1/variables.tf
@@ -1,1 +1,19 @@
+varibale "vpc_cidr" {
+    description = "CIDR block for VPC"
+    type        = string
+}
+variable "public_subnet_cidr" {
+  description    = "CIDR block for the public subnet"
+  type           = string
+}
+
+variable "az" {
+  description = "Availability zone for subnet"
+  type        = string
+}
+
+variable "project" {
+  description = "Project name for tagging"
+  type        = string
+}
 
