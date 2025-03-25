83676fac521575bfe9da4e974bc4e1336211590e
JhamelThorne <116086991+JhamelThorne@users.noreply.github.com>
2025-03-24 21:46:36 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,12 @@
 module "vpc" {
- source             = "./modules/vpc" #repo-root relative
+ source             = "./modules/vpc1" #repo-root relative
  vpc_cidr           = "10.0.0.0/16"
  public_subnet_cidr = "10.0.1.0/24"
  az                 = "us-east-1a"
  project            = "vpc-ec2-demo"
 }
 module "sg" {
- source  = "./modules/security_group"
+ source  = "./modules/security_group1"
  vpc_id  = module.vpc.vpc_id
  my_ip   = "98.81.156.246" # update ip address if necessary
  project = "vpc-ec2-demo"
