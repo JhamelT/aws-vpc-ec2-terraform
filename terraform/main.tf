71075f2281a2f2981eb77000c31ea907e363520c
JhamelThorne <116086991+JhamelThorne@users.noreply.github.com>
2025-03-26 22:33:44 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,25 @@
 module "ec2" {
     source = "/modules/ec2"
-    # Add any required variables here
+    variable "ami_id" {
+ description = "AMI ID for EC2 instance"
+ type        = string
+}
+variable "instance_type" {
+ description = "EC2 instance type"
+ type        = string
+}
+variable "subnet_id" {
+ description = "Public subnet ID"
+ type        = string
+}
+variable "sg_id" {
+ description = "Security group ID"
+ type        = string
+}
+variable "project" {
+ description = "Project tag"
+ type        = string
+}
     instance_type = "t2.micro"
     ami_id        = "ami-12345678"
     # Add other necessary inputs
