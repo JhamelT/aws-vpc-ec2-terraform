f3d2784bafd0106e22e45973ba73680d1323d67d
JhamelThorne <116086991+JhamelThorne@users.noreply.github.com>
2025-03-26 22:36:26 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,4 +18,24 @@ module "ec2" {
  subnet_id      = module.vpc.public_subnet_id
  sg_id          = module.sg.sg_id
  project        = "vpc-ec2-demo"
+variable "ami_id" {
+ description = "AMI ID for EC2 instance"
+ type        = string
+}
+variable "instance_type" {
+ description = "EC2 instance type"
+ type        = string
+}
+variable "subnet_id" {
+ description = "Public subnet ID"
+ type        = string
+}
+variable "sg_id" {
+ description = "Security group ID"
+ type        = string
+}
+variable "project" {
+ description = "Project tag"
+ type        = string
+}
 }
