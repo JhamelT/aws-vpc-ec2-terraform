e9a4afc860c06645cd94ecfc94ba44f93e3f7208
JhamelThorne <116086991+JhamelThorne@users.noreply.github.com>
2025-03-26 22:31:45 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,8 @@
+module "ec2" {
+    source = "./modules/ec2"
+    # Add any required variables here
+    instance_type = "t2.micro"
+    ami_id        = "ami-12345678"
+    # Add other necessary inputs
+}
 
