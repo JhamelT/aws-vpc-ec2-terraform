0708e15b0c0ea1c3722997f916ce7b53c3e4e82f
JhamelThorne <116086991+JhamelThorne@users.noreply.github.com>
2025-03-30 19:29:19 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,15 +8,15 @@ module "vpc1" {
 module "security_group1" {
  source  = "/modules/security_group1"
  vpc_id  = module.vpc.vpc_id
- my_ip   = "98.81.156.246" # update ip address if necessary
+ my_ip   = "98.84.152.240" # update ip address if necessary
  project = "vpc-ec2-demo"
 }
 module "ec2" {
  source         = "/modules/ec2"
- ami_id         = "ami-01f5a0b78d6089704"  # Use latest Amazon Linux 2 AMI - update ami if necessary
- instance_type  = "t3.micro"
- subnet_id      = module.vpc.public_subnet_id
- sg_id          = module.sg.sg_id
+ ami_id         = "ami-071226ecf16aa7d96"  # Use latest Amazon Linux 2 AMI - update ami if necessary
+ instance_type  = "t2.micro"
+ subnet_id      = module.vpc1.public_subnet_id
+ sg_id          = module.security_group1.sg_id
  project        = "vpc-ec2-demo"
 variable "ami_id" {
  description = "AMI ID for EC2 instance"
