ec0ea5e9bd16af20129aadf77955eb9efd3a1e7c
JhamelThorne <116086991+JhamelThorne@users.noreply.github.com>
2025-03-30 20:41:47 -0400

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
 output "ec2_public_ip" {
-  value = aws_instance.my_instance.public_ip
-  description = "Public IP of the EC2 instance"
+  value       = module.ec2.ec2_public_ip
+  description = " Public IP from EC2 Module"
 }
