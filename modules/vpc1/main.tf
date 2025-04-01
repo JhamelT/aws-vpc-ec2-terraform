67c63ac7a994e6c0bc94300fa14568525f7e5765
JhamelThorne <116086991+JhamelThorne@users.noreply.github.com>
2025-03-31 20:48:31 -0400

    Update main.tf

--- modules/vpc1/main.tf
+++ modules/vpc1/main.tf
@@ -1,6 +1,5 @@
 # Core Resources
-
-resource "aws_vpc" "main"  {
+resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
@@ -9,38 +8,46 @@ resource "aws_vpc" "main"  {
   }
 }
 
-resource "aws_internet_gateway" "igw"  {
+resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
   tags = {
     name = "${var.project}-igw"
   }
 }
+
 resource "aws_subnet" "public" {
-  vpc_id         = aws_vpc.main.id
-  cidr_block     = var.public_subnet_cidr
-  map_public_ip_on_launch  = true
-  availability_zone        =var.az
-  tags ={  
-      name = "${var.project}-public-subnet"
-      }
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+  availability_zone       = var.az
+  tags = {  
+    name = "${var.project}-public-subnet"
+  }
 }
 
 resource "aws_route_table" "public_rt" {
-    vpc_id = aws.vpc.main.id
-    tags = {
-      name = "{var.project}-public-rt"
-    }
+  vpc_id = aws_vpc.main.id
+  tags = {
+    name = "${var.project}-public-rt"
+  }
 }
 
 resource "aws_route" "default_route" {
- route_table_id         = aws_route_table.public_rt.id
- destination_cidr_block = "0.0.0.0/0"
- gateway_id             = aws_internet_gateway.igw.id
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
 }
 
 resource "aws_route_table_association" "public_assoc" {
- subnet_id      = aws_subnet.public.id
- route_table_id = aws_route_table.public_rt.id
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
 }
 
+# Required outputs for other modules
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
 
+output "public_subnet_id" {
+  value = aws_subnet.public.id
+}
