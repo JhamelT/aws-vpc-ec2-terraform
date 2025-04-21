ecaaed3ccad7197960204e73b0b4f9ab5d2ad824
Jha'Mel Thorne <116086991+JhamelThorne@users.noreply.github.com>
2025-04-20 20:00:34 -0400

    Update outputs.tf

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -2,3 +2,6 @@ output "ec2_public_ip" {
   value = aws_instance.web.public_ip
   description = "Public IP of the EC2 instance"
 }
+output "ec2_ssm_profile_name" {
+ value = aws_iam_instance_profile.ec2_ssm_profile.name
+}
